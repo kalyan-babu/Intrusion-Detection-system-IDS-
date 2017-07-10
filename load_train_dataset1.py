607e5e5af980a312ec45746d4fd724043915348f
kalyan <kalyanbuddavarapu@gmail.com>
2017-07-10 10:50:11 +0530

    pre-process the traindataset

---
+++ load_train_dataset1.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+
+
+
+columns=["duration","protocol_type","service","flag","src_bytes","dst_bytes","land","wrong_fragment","urgent","hot","num_failed_logins","logged_in","num_compromised","root_shell","su_attempted","num_root","num_file_creations","num_shells","num_access_files","num_outbound_cmds","is_host_login","is_guest_login","count","srv_count","serror_rate","srv_serror_rate","rerror_rate","srv_rerror_rate","same_srv_rate","diff_srv_rate","srv_diff_host_rate","dst_host_count","dst_host_srv_count","dst_host_same_srv_rate","dst_host_diff_srv_rate","dst_host_same_src_port_rate","dst_host_srv_diff_host_rate","dst_host_serror_rate","st_host_srv_serror_rate","dst_host_rerror_rate","dst_host_srv_rerror_rate"]
+
+raw_data=pd.read_csv(r'D:\python_projects\IDS-2\NSL_KDD\train.csv',names=columns)
+
+header=np.array(raw_data["dst_host_rerror_rate"])
+
+
+columns1=["service","flag","src_bytes","dst_bytes","land","wrong_fragment","urgent","hot","num_failed_logins","logged_in","num_compromised","root_shell","su_attempted","num_root","num_file_creations","num_shells","num_access_files","num_outbound_cmds","is_host_login","is_guest_login","count","srv_count","serror_rate","srv_serror_rate","rerror_rate","srv_rerror_rate","same_srv_rate","diff_srv_rate","srv_diff_host_rate","dst_host_count","dst_host_srv_count","dst_host_same_srv_rate","dst_host_diff_srv_rate","dst_host_same_src_port_rate","dst_host_srv_diff_host_rate","dst_host_serror_rate","st_host_srv_serror_rate","dst_host_srv_rerror_rate"]
+
+dataset=np.array(raw_data[columns1])
+
+
